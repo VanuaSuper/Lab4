000e73931edf62a24da12c214ae09fa55c45fd9e
VanuaSuper <vanuasdf@mail.ru>
2024-05-21 23:00:41 +0300

    Добавлен вывод общей информации о задании

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #4. GIT\n";
+    cout << "Variant #2. Conference program\n";
+    cout << "Author: Ivan Grechko\n";
     return 0;
 }
