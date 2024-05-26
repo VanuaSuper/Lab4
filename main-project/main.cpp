ac0fb0bd899288c2cb872811665b295c830f812a
VanuaSuper <vanuasdf@mail.ru>
2024-05-26 16:16:48 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #4. GIT\n";
     cout << "Variant #2. Conference program\n";
     cout << "Author: Ivan Grechko\n";
+    cout << "Ãğóïïà: 23ÏÈ1ä\n";
     return 0;
 }
